640ce2b38954a14cff2b06c82b50e55c3d04e4ff
Dario Scilipoti <dariosc@gmail.com>
2025-05-07 20:03:18 -0300

    Agregamos la clase 4 de Git Bash, esto en el readme.md

--- Python/Leccion1/Clase2-Variables.py
+++ Python/Leccion1/Clase2-Variables.py
@@ -11,3 +11,13 @@ print(id(x))
 # Las literales se escriben x240, la variable y = x984, la variable z = x304
 print(id(y))
 print(id(z))
+
+print(|x|) # Imprime el valor absoluto de x
+print(x + y) # Suma
+
+# Sumar dos números
+numero1 = 5
+numero2 = 7
+resultado = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", resultado)
+print
