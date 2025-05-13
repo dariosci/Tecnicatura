49836678537212ce240b90eb7721e50fad0bf2e3
Dario Scilipoti <dariosc@gmail.com>
2025-05-13 08:09:47 -0300

    Elimine lineas que no tenian que estar

--- Python/Leccion1/Clase2-Variables.py
+++ Python/Leccion1/Clase2-Variables.py
@@ -14,10 +14,3 @@ print(id(z))
 
 print(|x|) # Imprime el valor absoluto de x
 print(x + y) # Suma
-
-# Sumar dos números
-numero1 = 5
-numero2 = 7
-resultado = numero1 + numero2
-print("La suma de", numero1, "y", numero2, "es:", resultado)
-print
