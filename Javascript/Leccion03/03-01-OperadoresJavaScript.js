7b9e4033271f5d95868a950eff36e75db50ba994
Dario Scilipoti <dariosc@gmail.com>
2025-06-22 13:03:46 -0300

    comenzando Clase 8

--- Javascript/Leccion03/03-01-OperadoresJavaScript.js
+++ Javascript/Leccion03/03-01-OperadoresJavaScript.js
@@ -1,15 +1,26 @@
 // Ejercicio para encontrar números pares e impares
 let parImpar = 10;
 if (parImpar % 2 == 0) {
-    console.log("El número es PAR");
+  console.log("El número es PAR");
 } else {
-    console.log("El número es IMPAR");
+  console.log("El número es IMPAR");
 }
 
 //Ejercicio: es mayor de edad
-let edad = 18, adulto = 18;
-if (edad >= adulto){
-    console.log("Usted es una persona adulta");
+let edad = 18,
+  adulto = 18;
+if (edad >= adulto) {
+  console.log("Usted es una persona adulta");
 } else {
-    console.log("Usted es una persona menor de edad");
+  console.log("Usted es una persona menor de edad");
 }
+
+//Ejercicio: Dentro de un rango
+let dentroRango = 10;
+let valMin = 0,
+  valMax = 10;
+if (dentroRango >= valMin && dentroRango <= valMax) {
+  console.log("Está dentro del rango establecido");
+} else {
+  console.log("No está dentro del rango establecido");
+}ls
